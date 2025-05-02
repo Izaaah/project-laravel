7d63f5e56a4745df6578e9c1dd757cc2b9c44e1c
Izaah <izaahzaah@gmail.com>
2025-05-02 22:55:01 +0700

    add auth page

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard.master')
+
+@section('title', 'Registrasi')
+
+@section('content')
+    <div class="left-side">
+        <h1>TulisAja</h1>
+        {{-- <img src="{{ asset('storage/img/cover_regis.png') }}" alt="Buku dan Laptop"> --}}
+    </div>
+
+    <div class="right-side">
+        <div class="container">
+            <h2>Form Registrasi</h2>
+            <form action="/proses_register" method="POST">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Masukkan username" required>
+
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Masukkan email" required>
+
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Masukkan password" required>
+
+                <input type="submit" value="Daftar">
+                <p class="signup-link">Sudah punya akun? <a href="{{  route('login') }}">Login Sekarang</a></p>
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,12 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('users', UserController::class);
 });
 
+Route::get('/registrasi', function () {
+    return view('auth.register');
+})->name('registrasi');
+
+// Route::get('/registrasi', [auth::class, 'index'])->name('registrasi');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
