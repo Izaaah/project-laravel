c29f91e901543b27200f3227baeed488c47344cc
ultramunnn <munir63577@student.ub.ac.id>
2025-05-08 21:57:37 +0700

    Update Tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,25 +2,32 @@
 
 @section('title', 'Login')
 
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
 @section('content')
-    <div class="left-side">
-        {{-- <h1>TulisAja</h1> --}}
-        <img src="{{ asset('storage/img/cover_regis.png') }}" alt="Buku dan Laptop">
+<div class="left-side">
+    {{-- <h1>TulisAja</h1> --}}
+    <img src="{{ asset('storage/img/cover_regis.png') }}" alt="Buku dan Laptop">
+</div>
+
+
+
+
+<div class="right-side">
+    <div class="container">
+        <h2>Form Login</h2>
+        <form action="/proses_login" method="POST">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Masukkan username" required>
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Masukkan password" required>
+
+            <input type="submit" value="Daftar">
+            <p class="signup-link">Belum punya akun? <a href="{{  route('registrasi') }}">Daftar Sekarang</a></p>
+        </form>
     </div>
-
-    <div class="right-side">
-        <div class="container">
-            <h2>Form Login</h2>
-            <form action="/proses_login" method="POST">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" placeholder="Masukkan username" required>
-
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Masukkan password" required>
-
-                <input type="submit" value="Daftar">
-                <p class="signup-link">Belum punya akun? <a href="{{  route('registrasi') }}">Daftar Sekarang</a></p>
-            </form>
-        </div>
-    </div>
+</div>
 @endsection
