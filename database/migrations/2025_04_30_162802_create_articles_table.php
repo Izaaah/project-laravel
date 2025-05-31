4b2af90a3820e79b317db12e3c02682059be02a1
ultramunnn <munir63577@student.ub.ac.id>
2025-05-31 18:52:48 +0700

    fix:error migrate

--- database/migrations/2025_04_30_162802_create_articles_table.php
+++ database/migrations/2025_04_30_162802_create_articles_table.php
@@ -4,12 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-   // database/migrations/xxxx_xx_xx_create_articles_table.php
+    // database/migrations/xxxx_xx_xx_create_articles_table.php
     public function up()
     {
         if (!Schema::hasTable('articles')) {
@@ -22,7 +21,7 @@ return new class extends Migration
                 $table->enum('status', ['draft', 'published'])->default('draft');
                 $table->timestamps();
                 $table->foreignId('id_user')->constrained('users', 'id_user')->onDelete('cascade');
-                $table->foreignId('id_category')->constrained('categories')->onDelete('set null')->nullable();
+                $table->foreignId('id_category')->nullable()->constrained('categories')->onDelete('set null');
             });
         }
     }
